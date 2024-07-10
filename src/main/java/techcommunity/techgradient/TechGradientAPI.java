bc7438fde56f86bca2535b097d955575a6f99b7e
x3xgit <xyndex.shuvar@gmail.com>
2024-07-10 23:50:42 +0300

    1.0.1

--- src/main/java/techcommunity/techgradient/TechGradientAPI.java
+++ src/main/java/techcommunity/techgradient/TechGradientAPI.java
@@ -1,5 +1,11 @@
 package techcommunity.techgradient;
 
+import techcommunity.techgradient.gradient.GradientManager;
+
 public class TechGradientAPI
 {
+    public static String applyGradient(String textInput)
+    {
+        return GradientManager.apply(textInput);
+    }
 }
